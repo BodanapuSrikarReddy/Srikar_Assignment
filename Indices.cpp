97c9c99f27eacc63a5663191cadf5854258d6965
BodanapuSrikarReddy <102506553+BodanapuSrikarReddy@users.noreply.github.com>
2025-01-13 07:22:02 +0530

    Add files via upload

---
+++ Indices.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    int n = nums.size();
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (nums[i] + nums[j] == target) {
+                return {i, j};
+            }
+        }
+    }
+    return {}; 
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
+
+    vector<int> nums(n);
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    int target;
+    cout << "Enter the target value: ";
+    cin >> target;
+
+    vector<int> result = twoSum(nums, target);
+    if (!result.empty()) {
+        cout << "Indices: " << result[0] << ", " << result[1] << endl;
+    } else {
+        cout << "No solution found!" << endl;
+    }
+
+    return 0;
+}
